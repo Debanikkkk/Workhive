2011013cee6e6ede40dce40d0fc76e67a6295d73
Debanik Pahilwani <pahilwanidebanik@gmail.com>
2024-05-15 01:54:31 +0530

    path emp

--- src/tsoa-auto/routes.ts
+++ src/tsoa-auto/routes.ts
@@ -461,9 +461,6 @@ const models: TsoaRoute.Models = {
             "date_of_joining": {"dataType":"datetime","required":true},
             "username": {"dataType":"string"},
             "password": {"dataType":"string"},
-            "department": {"dataType":"double"},
-            "branch": {"dataType":"double"},
-            "company": {"dataType":"double"},
             "role": {"dataType":"double"},
             "skills": {"dataType":"array","array":{"dataType":"double"}},
         },
@@ -1219,7 +1216,7 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/employee/:employeeId',
+        app.get('/company/:companyId/branch/:branchId/department/:departmentId/employee/:employeeId',
             ...(fetchMiddlewares<RequestHandler>(EmployeeController)),
             ...(fetchMiddlewares<RequestHandler>(EmployeeController.prototype.getOneEmployee)),
 
@@ -1249,7 +1246,7 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/employee',
+        app.get('/company/:companyId/branch/:branchId/department/:departmentId/employee',
             authenticateMiddleware([{"Api-Token":[]}]),
             ...(fetchMiddlewares<RequestHandler>(EmployeeController)),
             ...(fetchMiddlewares<RequestHandler>(EmployeeController.prototype.getAllEmployeeBranch)),
@@ -1280,13 +1277,15 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.post('/employee',
-            authenticateMiddleware([{"Api-Token":[]}]),
+        app.post('/company/:companyId/branch/:branchId/department/:departmentId/employee',
             ...(fetchMiddlewares<RequestHandler>(EmployeeController)),
             ...(fetchMiddlewares<RequestHandler>(EmployeeController.prototype.saveEmployee)),
 
             async function EmployeeController_saveEmployee(request: ExRequest, response: ExResponse, next: any) {
             const args: Record<string, TsoaRoute.ParameterSchema> = {
+                    companyId: {"in":"path","name":"companyId","required":true,"dataType":"double"},
+                    branchId: {"in":"path","name":"branchId","required":true,"dataType":"double"},
+                    departmentId: {"in":"path","name":"departmentId","required":true,"dataType":"double"},
                     request: {"in":"body","name":"request","required":true,"ref":"ReqEmployee"},
                     req: {"in":"request","name":"req","required":true,"dataType":"object"},
             };
@@ -1312,7 +1311,7 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.post('/employee/login',
+        app.post('/company/:companyId/branch/:branchId/department/:departmentId/employee/login',
             ...(fetchMiddlewares<RequestHandler>(EmployeeController)),
             ...(fetchMiddlewares<RequestHandler>(EmployeeController.prototype.userLogin)),
 
@@ -1342,7 +1341,7 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.delete('/employee/:employeeId',
+        app.delete('/company/:companyId/branch/:branchId/department/:departmentId/employee/:employeeId',
             ...(fetchMiddlewares<RequestHandler>(EmployeeController)),
             ...(fetchMiddlewares<RequestHandler>(EmployeeController.prototype.deleteEmployee)),
 
